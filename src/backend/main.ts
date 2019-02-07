dd9ece0a9032a9c3c197222985d60aa1c84241cb
FrogTheFrog <warliukz@gmail.com>
2019-02-07 15:42:38 +0200

    throw unhandled expection so that process can exit

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -8,17 +8,9 @@ if (userDataDir !== undefined) {
         try {
             await AppManager.create(userDataDir, "steam-gyro.json");
         } catch (error) {
-            dialog.showMessageBox({
-                message: error,
-                title: "Error in Main process",
-                type: "error",
-            });
+            throw error;
         }
     })();
 } else {
-    dialog.showMessageBox({
-        message: "User directory is not set.",
-        title: "Error in Main process",
-        type: "error",
-    });
+    throw new Error("User directory is not set.");
 }
